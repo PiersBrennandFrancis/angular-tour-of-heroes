2290f4ddaddd770bf5b43be52b7b71cbdb2e9aa5
Piers Brennand-Francis <piers.brennand-francis@virginpulse.com>
2018-12-28 14:12:59 -0600

    Extracted detail functionality from heroes component.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes = HEROES;
 
-  hero = 'Windstorm';
+  selectedHero: Hero;
+
+  constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
